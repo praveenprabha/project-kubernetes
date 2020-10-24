97689351107526eeebaa02783da3594f94ed8043
Praveen P <praveen.prabha@gmail.com>
2020-10-24 20:39:43 +0000

    Creating DockerFile and associated files and folders to be copied

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -7,4 +7,17 @@ sed -i "s/username_here/$DB_USER/g" /var/www/html/wp-config.php
 sed -i "s/password_here/$DB_PASSWORD/g" /var/www/html/wp-config.php
 sed -i "s/localhost/$DB_HOST/g" /var/www/html/wp-config.php
 
+
+sed -i '/'"'WP_DEBUG'"'/a \
+// Cache all the things \
+global $memcached_servers; \
+$memcached_servers = array( \
+ array( \
+    "'"$MEMCAHED_HOST"'", \
+    "11211" \
+    ) \
+ ); \
+define( "WP_CACHE", true );' /var/www/html/wp-config.php
+
+
 apachectl -D FOREGROUND
