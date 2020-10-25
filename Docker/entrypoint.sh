a6d31554677255b6a2a2f0535855789df47e2bab
Praveen P <praveen.prabha@gmail.com>
2020-10-25 20:01:27 +0000

    Updated Docker to download the wordpress and plugin from external link than local files

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+
+
+#ls -al
+#ls -al /var/www/html/wp-content/
+#ls -al /var/www/html/wp-content/plugins
+
+
 cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 
 sed -i "s/database_name_here/$DB_NAME/g" /var/www/html/wp-config.php
@@ -19,5 +26,10 @@ $memcached_servers = array( \
  ); \
 define( "WP_CACHE", true );' /var/www/html/wp-config.php
 
+#exec /usr/local/bin/docker-entrypoint.sh apache2-foreground
+
+sed -i 's/\(var \$headers = array(\)\()\)/\1"memcached" => "activated-by-praveen"\2/g' /var/www/html/wp-content/advanced-cache.php
+
+chown -R nobody:nogroup /var/www/html
 
 apachectl -D FOREGROUND
